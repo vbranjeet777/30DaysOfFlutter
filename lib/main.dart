b835bbe5c92127ad3d10b32cc16bde4b118c4d58
“vbranjeet777” <kuranjeet777@gmail.com>
2024-08-27 19:34:38 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
-void main()
-{
+void main() {
   runApp(MyApp());
 }
 
@@ -11,10 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    
     return MaterialApp(
-      home:HomePage()
-    );
+        themeMode: ThemeMode.light,
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        initialRoute: "/home",
+        routes: {
+          "/": (context) => LoginPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+        });
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Text("Login Page"),
+    );
+  }
+}
