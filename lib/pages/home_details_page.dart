1aebfe138636f2a4eed66fd242058cdeb147b9d5
“vbranjeet777” <kuranjeet777@gmail.com>
2024-09-19 21:13:13 +0530

    day18 completed

--- lib/pages/home_details_page.dart
+++ lib/pages/home_details_page.dart
@@ -14,15 +14,67 @@ class HomeDetailsPage extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: Row(
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(padding: EdgeInsets.only(left: 20)),
+            Text(
+              "999",
+              textScaler: TextScaler.linear(1.5),
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Padding(
+                padding: EdgeInsets.only(left: 150.0, bottom: 80.0, right: 50)),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                " Buy ",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.deepPurple)),
+            ),
+            //Padding(padding: EdgeInsets.only(right: 0.1))
+          ],
+        ),
+      ),
       backgroundColor: MyTheme.creameColor,
       body: SafeArea(
+        bottom: false,
         child: Column(
           children: [
-            Image.asset("assets/iphone.webp").p16(),
+            Padding(padding: EdgeInsets.only(top: 15)),
+            Image.asset("assets/iphone.webp").h32(context),
             Expanded(
+              child: VxArc(
+                height: 30.0,
+                edge: VxEdge.top,
+                arcType: VxArcType.convey,
                 child: Container(
-              color: Colors.red,
-            ))
+                  width: context.screenWidth,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Text(
+                        "iPhone 12 Pro",
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Details of iPhone 12 pro",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      )
+                    ],
+                  ).py64(),
+                ),
+              ),
+            ),
           ],
         ),
       ),
