9d58322b5afd1ba81f598d9e23f6b418293a94c4
“vbranjeet777” <kuranjeet777@gmail.com>
2024-10-04 16:03:45 +0530

    day24 completed

--- lib/widgets/dartpratics.dart
+++ lib/widgets/dartpratics.dart
@@ -1,37 +1,22 @@
 import 'package:flutter/material.dart';
 
-
 class P_material_App extends StatelessWidget {
   const P_material_App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // super.key,
-    // this.appBar,
-    // this.body,
-    // this.floatingActionButton,
-    // this.floatingActionButtonLocation,
-    // this.floatingActionButtonAnimator,
-    // this.persistentFooterButtons,
-    // this.persistentFooterAlignment = AlignmentDirectional.centerEnd,
-    // this.drawer,
-    // this.onDrawerChanged,
-    // this.endDrawer,
-    // this.onEndDrawerChanged,
-    // this.bottomNavigationBar,
-    // this.bottomSheet,
-    // this.backgroundColor,
-    // this.resizeToAvoidBottomInset,
-    // this.primary = true,
-    // this.drawerDragStartBehavior = DragStartBehavior.start,
-    // this.extendBody = false,
-    // this.extendBodyBehindAppBar = false,
-    // this.drawerScrimColor,
-    // this.drawerEdgeDragWidth,
-    // this.drawerEnableOpenDragGesture = true,
-    // this.endDrawerEnableOpenDragGesture = true,
-    // this.restorationId,
-    // );
+    return MaterialApp(
+      home: Center(
+          child: RichText(
+              text: TextSpan(
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.amberAccent),
+                  children: <InlineSpan>[
+            TextSpan(text: "Rajaa"),
+            TextSpan(text: "kumar"),
+          ]))),
+    );
   }
 }
