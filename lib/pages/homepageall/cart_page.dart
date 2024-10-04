3ceadaf825d13b04e4e71b697edf27d189fa2bbb
“vbranjeet777” <kuranjeet777@gmail.com>
2024-10-04 16:15:12 +0530

    day25 completed

--- lib/pages/homepageall/cart_page.dart
+++ lib/pages/homepageall/cart_page.dart
@@ -15,6 +15,24 @@ class CartPage extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
+      body: Center(
+          child: RichText(
+              text: TextSpan(
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black),
+                  children: <InlineSpan>[
+            TextSpan(
+                text: "Ranjeet ",
+                style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.redAccent)),
+            TextSpan(
+              text: "Kushwaha",
+            ),
+          ]))),
     );
   }
 }
--- lib/widgets/dartpratics.dart
+++ lib/widgets/dartpratics.dart
@@ -1,22 +1,25 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class P_material_App extends StatelessWidget {
-  const P_material_App({super.key});
+// class P_material_App extends StatelessWidget {
+//   const P_material_App({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(
-          child: RichText(
-              text: TextSpan(
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.amberAccent),
-                  children: <InlineSpan>[
-            TextSpan(text: "Rajaa"),
-            TextSpan(text: "kumar"),
-          ]))),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Center(
+//           child: RichText(
+//               text: TextSpan(
+//                   style: TextStyle(
+//                       fontSize: 20,
+//                       fontWeight: FontWeight.normal,
+//                       color: Colors.amberAccent),
+//                   children: <InlineSpan>[
+//             TextSpan(text: "Rajaa"),
+//             TextSpan(text: "kumar"),
+//           ]
+//           )
+//           )
+//           ),
+//     );
+//   }
+// }
